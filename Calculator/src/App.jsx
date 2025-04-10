18a3d865a6b724b7b4d3987b77b697d956a1f9ed
Kishen S Jain <jain.skishen@gmail.com>
2025-04-10 21:18:23 +0530

    Added basic UI and logic

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,33 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('');
+  const handleClick = (value) => {
+    if(value === 'C') {
+      setInput('');
+    }else if(value === '=') {
+      try{
+        setInput(eval(input).toString());
+      }catch{
+        setInput('Error');
+      }
+    }else{
+      setInput(input + value);
+    }
+  }
+
+  const buttons = ['7', '8', '9', 'C', '4', '5', '6', '*', '1', '2', '3', '+', '0', '.', '=', '/'];
+  const buttonClass = 'bg-gray-800 p-2 rounded-lg cursor-pointer hover:bg-gray-700 hover:scale-105 transition-all duration-200 w-12';
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="w-full max-w-sm mx-auto mt-20 p-6 bg-black text-white rounded-2xl shadow-lg flex flex-col items-center">
+        <h1 class="text-2xl font-bold mb-4">Calculator</h1>
+        <input
+          type='text'
+          className='w-full p-2 bg-gray-800  text-white rounded-2xl shadow-lg mb-4'
+          value={input}
+          readOnly
+        />
+        <div className='grid grid-cols-4 gap-4'>
+          {buttons.map((btn) => (
+             <button
+             key={btn}
+             className={`${btn === 'C' ? 'bg-red-500 hover:bg-red-600' : buttonClass}`}
+             onClick={() => handleClick(btn)}
+           >
+            {btn}
+            </button>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
