1b03d279e0e91fb824c21024e55fd12331d3c79d
Kishen S Jain <jain.skishen@gmail.com>
2025-04-14 21:15:02 +0530

    listed tasks

--- 02.to-do list/src/App.jsx
+++ 02.to-do list/src/App.jsx
@@ -1,11 +1,40 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('')
+  const [tasks, setTasks] = useState([])
+  
+  const handleInput = () =>{
+    if (input.trim() !== '') {
+      console.log('Task added:', input)
+      setTasks([...tasks, input])
+      setInput('')
+    } else {
+      prompt('Please enter a task!')
+    }
+  }
 
   return (
     <>
-      
+      <h1>To-Do List</h1>
+      <div className='container'>
+        <input 
+          type="text" 
+          placeholder='Add a task...'
+          className="input"
+          onChange={(e)=> setInput(e.target.value)} 
+          autoFocus
+          />
+          <button 
+            className='btn add'
+            onClick={handleInput}
+            >Add</button>
+      </div>
+      <ul>
+      {tasks.map((task, index) => (
+        <li key={index}>{task}</li>
+      ))}
+    </ul>
     </>
   )
 }
