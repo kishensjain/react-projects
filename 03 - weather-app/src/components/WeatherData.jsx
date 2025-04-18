026c9833b819338437032eb84e4c740a02a8a362
Kishen S Jain <jain.skishen@gmail.com>
2025-04-18 15:33:49 +0530

    🌦️ Display weather info: description, temp, humidity, wind speed

--- 03 - weather-app/src/components/WeatherData.jsx
+++ 03 - weather-app/src/components/WeatherData.jsx
@@ -2,17 +2,39 @@ import React from 'react'
 
 function WeatherData({data}) {
   if(!data || Object.keys(data).length === 0){
-    return <p>🔍 No weather data to display yet.</p>;
+    return <p className='noData'>🔍 No weather data to display yet.</p>;
   }
 
-  if (data === null) {
-    return <p>❌ Failed to fetch weather data. Please try again.</p>;
-  }
+  const{
+    name,
+    weather = [],
+    main = {},
+    wind = {}
+  } = data;
+
+  const description = weather[0].description ?? 'N/A';
+  const iconCode = weather[0]?.icon;
+  const iconUrl = iconCode ? `https://openweathermap.org/img/wn/${iconCode}@2x.png` : null;
+  const temperature = main.temp ?? 'N/A';
+  const humidity = main.humidity ?? 'N/A';
+  const windSpeed = wind.speed ?? 'N/A';
 
   return (
-    <div className='weatherData'>
-      <p>📍 {data.name}</p>
-    </div>
+    <section className='weatherData'>
+      {/* <p><strong className='label'>📍City: </strong>{name}</p> */}
+      <h2>🌦️ Weather in {name}</h2>
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt={description}
+          className="weather-icon"
+        />
+      )}
+      <p><strong className='label'>🌤️ Weather: </strong>{description}</p>
+      <p><strong className='label'>🌡️ Temperature: </strong>{temperature} °C</p>
+      <p><strong className='label'>💧 Humidity: </strong>{humidity} %</p>
+      <p><strong className='label'>💨 Wind speed: </strong>{windSpeed} m/s</p>
+    </section>
   )
 }
 
