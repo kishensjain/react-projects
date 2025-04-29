8f816914f1a4d2ca13361a7079bd7a7cd73d54fc
Kishen S Jain <jain.skishen@gmail.com>
2025-04-29 16:15:17 +0530

    finish random-bg

---
+++ mini-projects/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import RandomBgColor from './components/RandomBgColor'
+
+function App() {
+
+  return (
+    <>
+      <RandomBgColor/>
+    </>
+  )
+}
+
+export default App
