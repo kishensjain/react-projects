db18577986c2cf324ca420021ab97236ad8141ba
Kishen S Jain <jain.skishen@gmail.com>
2025-04-30 20:45:40 +0530

    add data.js

---
+++ 05 - movie-ratings/src/data.js
@@ -0,0 +1,29 @@
+const movies = [
+  {
+    id: 1,
+    name: "The Shawshank Redemption",
+    description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+  },
+  {
+    id: 2,
+    name: "The Godfather",
+    description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+  },
+  {
+    id: 3,
+    name: "The Dark Knight",
+    description: "When the menace known as the Joker emerges, he creates chaos in Gotham, forcing Batman closer to crossing the line between hero and vigilante."
+  },
+  {
+    id: 4,
+    name: "Pulp Fiction",
+    description: "The lives of two mob hitmen, a boxer, and a pair of bandits intertwine in a series of bizarre, often darkly comedic incidents."
+  },
+  {
+    id: 5,
+    name: "Inception",
+    description: "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO."
+  }
+];
+
+export default movies;
